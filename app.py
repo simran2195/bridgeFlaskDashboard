42e38562fcd6666a4ebce7678208b6767b3588c0
simran2195 <simran13104@iiitd.ac.in>
2023-02-26 17:23:56 -0800

    changes

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Load data from CSV
-    df = pd.read_csv('data.csv')
+    # df = pd.read_csv('data.csv')
+    df = pd.read_csv('/Users/admin/Desktop/UW MSTI/Quarter 2/514/project/bridgeFlaskDashboard/data/mlt_20170619_083708_300.csv')
+    data = df.loc[:, ['Timestamp', 'accelZ', 'Lat', 'Long']]
+    # Begin by visualizing the z-axis acceleration:
+    z_acc = data[["Timestamp", "accelZ"]]
+    z_acc.plot(x = "Timestamp", y = "accelZ", kind = "line", figsize=(20, 8), xlabel = None)
+    plt.show()
     
     # Generate plot
-    plt.plot(df['x'], df['y'])
     plt.xlabel('X')
     plt.ylabel('Y')
     
